6ccae74c449a323f9b8ff5967d908a2831253275
AbdulNaveed Soudagar <abdulnaveedsoudagar@AbdulNaveeds-MacBook-Pro.local>
2018-03-20 23:41:29 +0530

    Initial Commit

---
+++ MSCalendar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MSCalendar
+//
+//  Created by AbdulNaveed Soudagar on 20/03/18.
+//  Copyright © 2018 AbdulNaveed Soudagar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
